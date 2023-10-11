b48aba2de3715c68baed25f7d4a3c58cb944c17d
SebAstiAnP2137 <polak-sebastian2004@o2.pl>
2023-10-11 13:25:41 +0200

    opisik

--- wyszukiwanie_adresu_emailowego.php
+++ wyszukiwanie_adresu_emailowego.php
@@ -1,6 +1,6 @@
 <?php
 $wzorzec = '/[-\w.]+@([-A-z0-9]+\.)+[A-z]/';
-$tekst = 'dzonfasoleczka@wp.pl'
+$tekst = "email : dzonfasoleczka@wp.pl";
 if( preg_match ($wzorzec,$tekst))
     echo "znaleziono email: ";
 else
