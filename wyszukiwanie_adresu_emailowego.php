9d6f65c6373e79b3ea2c724904478383e92b7d47
SebAstiAnP2137 <polak-sebastian2004@o2.pl>
2023-10-11 13:36:20 +0200

    grzybiarze

--- wyszukiwanie_adresu_emailowego.php
+++ wyszukiwanie_adresu_emailowego.php
@@ -1,8 +1,8 @@
 <?php
 $wzorzec = '/[-\w.]+@([-A-z0-9]+\.)+[A-z]/';
-$tekst = "email : dzonfasoleczka@wp.pl";
-if( preg_match ($wzorzec,$tekst))
-    echo "znaleziono email: ";
+$tekst ="email : dzonfasoleczka@wp.pl";
+if( preg_match ($wzorzec,$tekst,$matches))
+    echo "znaleziono email: " .$matches[0];
 else
     echo "nie znaleziono emailu: ";
 ?>
---
+++ wyszukiwanie_adresu_www.php
@@ -0,0 +1,8 @@
+<?php
+$wzorzec = '/((\bhtts?:\/\/)|(\bwww\.))\S*/';
+$tekst ="www.forumgrzybiarskie.pl";
+if( preg_match ($wzorzec,$tekst,$matches))
+    echo "znaleziono strone: " .$matches[0];
+else
+    echo "nie znaleziono strony ";
+?>
