188cedce92e126df4de01ab0a32062e677b9e42e
SebAstiAnP2137 <polak-sebastian2004@o2.pl>
2023-10-19 09:54:14 +0200

    ceate_table_sq

--- create_database.php
+++ create_database.php
@@ -1,41 +1,65 @@
-<?php 
-    $servername="localhost";
-    $username="root";
-    $password="";
-    
-    //Create connection 
-    $conn=mysqli_connect($servername,$username,$password);
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$db="inb5_2";
 
-    //Check connection
-    if (!$conn){
-        die("Connection failed: " .mysqli_connect_error());
-    }
-    echo "connection sucessfully";
+
+$conn=mysqli_connect($servername,$username,$password,$db);
+if(!$conn){
+    die("connection failed: ".mysqli_connect_error());
+}
+echo "connected successfully";
 
 
 $database="INB5_2";
-//Create database
-$sql = "CREATE DATABASE $database";
-if(mysqli_query($conn,$sql)){
-    echo "Database created sucessfully";
-}else {
-"Error creating database : ".mysqli_error($conn);
-}
-$sql= "CREATE TABLE MyGuests (
-                                id INT(6) UNSIGNED AUTO_INCREMENT
-                                        PRIMARY KEY,
-                                firstname VARCHAR(30) NOT NULL,
-                                lastname VARCHAR(40) NOT NULL,
-                                E-mail VARCHAR(50),
-                                reg_date TIMESTAMP DEFAULT
-                                        CURRENT_TIMESTAMP ON UPDATE
-                                    CURRENT_TIMESTAMP
-                                )";
+$sql="CREATE DATABASE $database";
 if (mysqli_query($conn,$sql)){
-    echo "Table MyGuests created successfully";
-}else {
-    echo "error creating table: ".mysqli_error($conn);
+    echo "database create successfully";
+}else{
+    echo "error crating database: ".mysqli_error($conn);
+}
+$sql="CREATE TABLE MyGuests(
+    id INT(6) UNSIGNED AUTO_INCREMENT
+        PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP DEFAULT
+            CURRENT_TIMESTAMP ON UPDATE
+        CURRENT_TIMESTAMP
+)";
+if (mysqli_query($conn,$sql)){
+    echo "table MyGuests create successfully";
+}else{
+    echo "error crating table: ".mysqli_error($conn);
+}
+
+$sql="INSERT INTO MyGuests (firstname,lastname,email)
+                VALUES ('Krystian', 'Ronaldo' , 'kr007@o2.pl');";   
+$sql.="INSERT INTO MyGuests (firstname,lastname,email)
+                VALUES ('Leonard', 'Messi' , 'pepsicola@o2.pl');";
+$sql.="INSERT INTO MyGuests (firstname,lastname,email)
+                VALUES ('Kamil', 'De Brujne' , 'kdbrujne007@o2.pl');";
+$sql.="INSERT INTO MyGuests (firstname,lastname,email)
+                VALUES ('Sebastian', 'Alwarez' , 'plytkisebekglazurnik@wp.pl');";
+if (mysqli_multi_query($conn,$sql)){
+    echo "Records in MyGuests create successfully";
+}else{
+    echo "error creating records: ".mysqli_error($conn);
+}
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = my_sqli_query($conn,$sql);
+
+if(my_sqli_num_rows($result)>0){
+    while($row = mysqli_fetch_assoc($result)){
+        echo :"id: ". $row["id"]. " - Name: ".
+        $row["firstname"]. " ".$row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
 }
 
 mysqli_close($conn);
+
 ?>
